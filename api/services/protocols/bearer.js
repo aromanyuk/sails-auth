3089d2ed0057b8c4bc9806977913e5586f46f0ac
Ville Määttä <ville.maatta@ruriat.com>
2014-12-05 21:24:19 +0200

    Look for token in AccessToken (OAuth2orize)

--- api/services/protocols/bearer.js
+++ api/services/protocols/bearer.js
@@ -3,41 +3,6 @@
  * a bearer token contained in the `Authorization` header field, `access_token`
  * body parameter, or `access_token` query parameter.
  *
- * Applications must supply a `verify` callback, for which the function
- * signature is:
- *
- *     function(token, done) { ... }
- *
- * `token` is the bearer token provided as a credential.  The verify callback
- * is responsible for finding the user who posesses the token, and invoking
- * `done` with the following arguments:
- *
- *     done(err, user, info);
- *
- * If the token is not valid, `user` should be set to `false` to indicate an
- * authentication failure.  Additional token `info` can optionally be passed as
- * a third argument, which will be set by Passport at `req.authInfo`, where it
- * can be used by later middleware for access control.  This is typically used
- * to pass any scope associated with the token.
- *
- * Options:
- *
- *   - `realm`  authentication realm, defaults to "Users"
- *   - `scope`  list of scope values indicating the required scope of the access
- *              token for accessing the requested resource
- *
- * Examples:
- *
- *     passport.use(new BearerStrategy(
- *       function(token, done) {
- *         User.findByToken({ token: token }, function (err, user) {
- *           if (err) { return done(err); }
- *           if (!user) { return done(null, false); }
- *           return done(null, user, { scope: 'read' });
- *         });
- *       }
- *     ));
- *
  * For further details on HTTP Bearer authentication, refer to [The OAuth 2.0 Authorization Protocol: Bearer Tokens](http://tools.ietf.org/html/draft-ietf-oauth-v2-bearer)
  *
  * @constructor
@@ -45,10 +10,16 @@
  * @param {string}   token
  * @param {Function} next
  */
-module.exports = function (req, token, next) {
-    User.findOne({ token: token }, function (err, user) {
+module.exports = function (req, accessToken, next) {
+    AccessToken.findOne({ token: accessToken }, function(err, token) {
+
         if (err) { return next(err); }
-        if (!user) { return next(null, false); }
-        return next(null, user, { scope: 'all' });
+        if (!token) { return next(null, false); }
+
+        User.findOne({id: token.userId}, function(err, user) {
+            if (err) { return next(err); }
+            if (!user) { return next(null, false); }
+            return next(null, user, { scope: 'all' });
+        });
     });
 };
